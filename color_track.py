7952900920f4493f9de16dd50e593b4c12684a6b
Allen <AllenShielder1994@gmail>
2020-03-19 01:49:33 +1300

    update new code--git_HSV.py and new function--coordinate track in color_track.py

--- color_track.py
+++ color_track.py
@@ -7,8 +7,8 @@ from cv2 import cv2
 #upper = np.array([77,255,255])
 
 #RED
-lower = np.array([156,43,46])
-upper = np.array([180,255,255])
+#lower = np.array([156,43,46])
+#upper = np.array([180,255,255])
 
 #YELLOW
 #lower = np.array([26,43,46])
@@ -18,9 +18,21 @@ upper = np.array([180,255,255])
 #lower = np.array([78,43,46])
 #upper = np.array([124,255,255])
 
+#Gitted_red
+lower =np.array([145,130,160])
+upper =np.array([180,255,255])
+
+
+
+
 green = (0, 255, 0)
 blue  = (255, 0, 0)
 red   = (0, 0, 255)
+
+x_colllect =0
+y_colllect =0
+time =1
+current_position =0
 
 def show_distance (trgt_length,focallength,per_length):
     
@@ -43,28 +55,70 @@ while True:
     mask = cv2.inRange(hsv_frame,lower,upper)
     cv2.imshow ("mask", mask)
     conts,hier = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)#找出边界
-    cv2.drawContours(img,conts,-1,blue,1)#画出边框
+    cv2.drawContours(img,conts,-1,blue,2)#画出边框
     dst = cv2.bitwise_and(img,img,mask=mask)#对每一帧进行位与操作，获取追踪图像的颜色
     cv2.imshow ("dst",dst)
     cv2.imshow ("img2",img)
+
+   
 
     for i in range(0,len(conts)):  
         x, y, w, h = cv2.boundingRect(conts[i])   
         cv2.rectangle(img, (x,y), (x+w,y+h), green, 2) 
         #show_distance (10,20,h)
+
+    check_time =11
+    correction_value =5
+       
+    if time <check_time:
+        #print (time)
+        x_colllect = x_colllect + x
+        # print (x_colllect,x)
+        time=time+1
+    
+    else :
+    # print ("result:")
+        if ((x_colllect / check_time) < (current_position-correction_value)) or ((x_colllect / check_time) > (current_position+correction_value)):
+            current_position = x_colllect/check_time
+            #print (current_position, x_colllect)
+            print (current_position)
+            #print ("END:")
+        x_colllect = 0
+        time =1
+
+    
+  
+
+    key =cv2.waitKey(1) #保持画面的持续。
+    if key == ord ("Q"):
+        if time <check_time:
+            #print (time)
+            x_colllect=x_colllect+x
+           # print (x_colllect,x)
+            time=time+1
+
+        else :
+            # print ("result:")
+            print (x_colllect/check_time)
+            #print ("END:")
+            x_colllect = 0
+            time =1
+            
+    elif key == ord ("W"):
+        print ("**************************** END X",check_time, "****************************")
+        break
         
 
     cv2.imshow("img",img)
 
 
-    k=cv2.waitKey(1) #保持画面的持续。
-    if k == 27:
+    if key == 27:
         #通过esc键退出摄像
         cv2.destroyAllWindows()
         break
-    elif k==ord("S"):
+    elif key ==ord("S"):
         #通过s键保存图片，并退出。
-        cv2.imwrite("image2.jpg",img)
+        cv2.imwrite("image3.jpg",img)
         cv2.destroyAllWindows()
         break
         
